226773c0dc35c0a0e70ff0950d13b00717b2dc3a
Bolo Lai <cantonbolo@gmail.com>
2018-05-04 01:16:05 +0800

    dev

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <h5 class="card-header">Featured</h5>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    <img src="./assets/coding.svg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" src="~bootstrap/scss/bootstrap.scss">
+</style>
